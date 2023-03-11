bb16d19b1f102ef19d1a0e99758ec2a3beff4342
RGlovesyou <68345835+SimplyRed900@users.noreply.github.com>
2023-03-11 14:27:28 -0500

    Create Aircraft class

---
+++ Air_Traffic_Control_System/Aircraft.h
@@ -0,0 +1,26 @@
+/*
+ * Aircraft.h
+ *
+ *  Created on: Mar 11, 2023
+ *      Author: Argi
+ */
+
+#ifndef AIRCRAFT_H_
+#define AIRCRAFT_H_
+
+class Aircraft {
+public:
+	Aircraft();
+
+	virtual ~Aircraft();
+
+private:
+	int angle;
+	int distance;
+	int flightId;
+	int flightLevel;
+	int speed;
+	int position;
+};
+
+#endif /* AIRCRAFT_H_ */
