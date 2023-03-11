8d4da9725ef246df88d5429656a5ffaa339e9761
Liam Pereira <pereira.l.h@hotmail.ca>
2023-03-11 15:46:41 -0500

    Create PSR function prototypes

--- Air_Traffic_Control_System/src/PSR.h
+++ Air_Traffic_Control_System/src/PSR.h
@@ -7,11 +7,31 @@
 
 #ifndef PSR_H_
 #define PSR_H_
+#include "Aircraft.h"
 
 class PSR {
 public:
 	PSR();
+
+	void scan();
+	void emitPulses();
+	void roateRadar();
+	bool receiveEchoes(int echoes[]);
+	Aircraft* determineDistances(int echoes[]);
+
+	void sendAircraftPositionsToCS(Aircraft aircraft[]);
+	void sendAircraftPositionsToSSR(Aircraft aircraft[]);
+
 	virtual ~PSR();
+
+private:
+
+	int range;
+	int refernce_angle;
+	int scans_per_minute;
+	int pulses_per_degree;
+	int current_angle;
+	int pulse_speed;
 };
 
 #endif /* PSR_H_ */
