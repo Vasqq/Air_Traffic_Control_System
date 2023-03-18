faa2eb0601f364e311ee7d3121f3006d4cb7ad9e
RGlovesyou <68345835+SimplyRed900@users.noreply.github.com>
2023-03-18 11:43:33 -0400

    Added Code to test with PSR and Aircraft

--- Air_Traffic_Control_System/src/SSR.cpp
+++ Air_Traffic_Control_System/src/SSR.cpp
@@ -7,28 +7,73 @@
 
 #include "SSR.h"
 #include "Aircraft.h"
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+
 SSR::SSR() {
 	// TODO Auto-generated constructor stub
 
 };
 
+/*Input: Aircraft targetAircraft
+ *Output: Aircraft targetAircraft
+ *Description:This method will request the aircraft thread for transponder data
+ */
 void SSR::interrogateAircraft(Aircraft targetAircraft){
 
+
+	Aircraft A;
+
+
 };
 
-void SSR:: receiveTransponderData(Aircraft targetAircraft){
+/*Input: Aircraft targetAircraft
+ *Output: Aircraft targetAircraft
+ *Description:This will receive transponder data between Aircraft Thread and SSR.
+ */
+SSR::transponderData SSR::receiveTransponderData(Aircraft targetAircraft){
 
+	Aircraft A;
+	//Aircraft Flight ID and Flight Level
+	TD.flightID=A.getFlightId(targetAircraft);//after we have successfully interrogated the Aircrafts in illuminatedObjects, we can go ahead and grab the required info
+	TD.flightLevel= A.getFlightLevel(targetAircraft);
+
+	//Aircraft Positions X,Y,Z
+	TD.positionX=A.getPositionX(targetAircraft);
+	TD.positionY=A.getPositionY(targetAircraft);
+	TD.positionZ=A.getPositionZ(targetAircraft);
+
+	//Aircraft Speeds X,Y,Z
+	TD.speedX=A.getSpeedX(targetAircraft);
+	TD.speedY=A.getSpeedY(targetAircraft);
+	TD.speedZ=A.getSpeedZ(targetAircraft);
+	return TD;
 };
 
-Aircraft* SSR::Interrogate(Aircraft illuminatedObjects[])
+ SSR::transponderData* SSR::Interrogate(Aircraft illuminatedObjects[])		//first we receive an array of illuminated Objects (assume we push them from back to front)
 {
-    return nullptr;
+	int i;
+	int s= (sizeof(illuminatedObjects) / sizeof((illuminatedObjects[0])));		//because I refuse to run a for loop for this, I'm using this to get the length
+	for (i=0;i<=s;i++){
+		//interrogate the aircraft and receive the transponder data
+		interrogateAircraft(illuminatedObjects[i]);		//goes into array and grabs ith object and passes it to function
+				//if we successfully ask for the
+		//Store this inside an Aircraft object array called transponderData to use for the senTransponderData function
+
+	transponderDataArr[i]=receiveTransponderData(illuminatedObjects[i]);		//doing this will also dynamically increase the size of the array
+
+	}
+	return transponderDataArr;
 }
 
 
-Aircraft* SSR::sendTransponderData(Aircraft transponderData[])
+ SSR::transponderData* SSR::sendTransponderData(transponderData transponderDataArr[])
 {
-    return nullptr;
+
+	 return transponderDataArr;
 }
 SSR::~SSR() {
 	// TODO Auto-generated destructor stub
--- Air_Traffic_Control_System/src/SSR.h
+++ Air_Traffic_Control_System/src/SSR.h
@@ -10,16 +10,40 @@
 #include "Aircraft.h"
 
 class SSR {
+
+private:
+	struct transponderData
+{
+			int flightId;
+			int flightLevel;
+			int positionX;
+			int positionY;
+			int positionZ;
+			int speedX;
+			int speedY;
+			int speedZ;
+
+ };
+
+
+ transponderData TD;
+
 public:
 	SSR();
 
-	Aircraft* Interrogate(Aircraft illuminatedObjects[]);
-	Aircraft* sendTransponderData(Aircraft transponderData[]);
+		transponderData transponderDataArr[5];
 
+
+		transponderData* Interrogate(Aircraft illuminatedObjects[]);
+		transponderData* sendTransponderData(transponderData transponderData[]);
+		transponderData receiveTransponderData(Aircraft targetAircraft);
 	void interrogateAircraft(Aircraft targetAircraft);
-	void receiveTransponderData(Aircraft targetAircraft);
+
 
 	virtual ~SSR();
+
+
+
 };
 
 #endif /* SSR_H_ */
